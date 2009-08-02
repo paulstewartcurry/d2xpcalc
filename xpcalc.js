2aba1ced5804a11ac708228a1285734c0351cc96
Paul Curry <dashteacup@comcast.net>
2009-08-02 14:19:10 -0500

    first version of the script The function makeTable isn't being called, or is being called wrongly. Its alert isn't playing.  I tried using a simple function and the alert worked. Strange.

--- xpcalc.js
+++ xpcalc.js
@@ -1,1 +1,25 @@
-// Stuff goes here
+var areaLevelsAndXP = [
+    ["Blood Moor ",1,24,36,2100,67,21400],
+    ["Den of Evil ",1,29,36,2600,79,43500],
+    ["Cold Plains ",2,30,36,2500,68,27200],
+    ["Cave Level 1 ",2,44,36,2500,77,39000],
+    ["Cave Level 2 ",2,44,37,2700,78,40300],
+    ["Burial Grounds ",3,39,36,2400,80,42200],
+    ["Crypt ",3,39,37,2600,83,46100]
+];
+
+function makeTable() {
+    var tableDiv = Document.getElementById("table");
+    var tableText = "<table>";
+    alert("this is an alert");
+    for (elem in areaLevelsAndXP) {
+        tableText += "<tr>";
+        for (entry in elem) {
+            tableText += "<td>" + entry + "</td>";
+        }
+        tableText += "</tr>";
+    }
+    tableText += "</table>";
+    tableDiv.innerHTML = tableText;
+}
+
