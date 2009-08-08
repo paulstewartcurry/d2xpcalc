3031800e2fcbe8cfef9e882b21e5b5c593289f54
Wes Oldenbeuving <narnach@gmail.com>
2009-08-08 13:29:35 +0200

    Remove all things that are not real monsters from the MonStats list

--- mon_stats.rb
+++ mon_stats.rb
@@ -1,5 +1,9 @@
 require 'datasource'
 
+# Monstats has a couple of interesting columns:
+# * Id corresponds with the 'monN' columns from Levels and will be used as a lookup key.
+# * Exp is the percentage points of experience the monster gains relative to a monster of its level, defined in MonLvl.
+# * Level is the monster's level, though it is ignored in Nightmare and Hell in favor of the area level.
 class MonStats < Datasource
   attr_accessor :monsters
 
@@ -8,17 +12,20 @@ class MonStats < Datasource
     @monsters = []
     rows.each do |row|
       levels = %w[Level Level(N) Level(H)].map {|col| value(row,col)}.compact.map {|n| n.to_i}
-      next if levels.empty?
+      exp = %w[Exp Exp(N) Exp(H)].map {|col| value(row,col)}.compact.map {|n| n.to_i}
+      name = value(row,'NameStr')
+      next if levels.empty? or exp.empty? or name.nil?
       monsters << {
         :id => value(row, 'Id'),
-        :name => value(row,'NameStr'),
+        :name => name,
         :levels => levels,
+        :exp_pct => exp,
       }
     end
   end
   
   def to_s
-    monsters.sort {|a,b| a[:id] <=> b[:id]}.map {|row| row.inspect}.join("\n")
+    monsters.sort {|a,b| a[:levels] <=> b[:levels]}.map {|row| row.inspect}.join("\n")
   end
 end
 
